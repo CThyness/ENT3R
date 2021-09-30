66fb5b3eeb07542d1046b79d3799b01cacb932da
carl <carl.thyness@gmail.com>
2021-09-30 07:26:05 +0200

    Update Rapportfiks.py

--- Rapportfiks.py
+++ Rapportfiks.py
@@ -5,6 +5,9 @@ f.readline() #Bli kvitt første linje som bare inneholder
 guests = []
 for line in f:
     values = line.split(",")
+
+    if values[0] == '"':
+        values[0] = values[0][1:-1]
     if values[0].lower() not in guests and values[2] != "Moderator": #Sjekker at eleven ikke er i listen eller moderator
         guests.append(values[0].lower())
 
