766e469d0a661a2489ae756529c3f2e446372cad
carl <carl.thyness@gmail.com>
2021-09-30 07:28:05 +0200

    Update Rapportfiks.py

--- Rapportfiks.py
+++ Rapportfiks.py
@@ -6,8 +6,9 @@ guests = []
 for line in f:
     values = line.split(",")
 
-    if values[0] == '"':
+    if values[0] == '"': #Sjekker om navnet har fått " om seg og eventuelt fjerner det
         values[0] = values[0][1:-1]
+
     if values[0].lower() not in guests and values[2] != "Moderator": #Sjekker at eleven ikke er i listen eller moderator
         guests.append(values[0].lower())
 
