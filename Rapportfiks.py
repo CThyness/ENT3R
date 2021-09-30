d928a0b9d728f33b2dc23027236fddbc2d5e7e41
carl <55590105+CThyness@users.noreply.github.com>
2021-09-30 07:13:43 +0200

    Add files via upload

---
+++ Rapportfiks.py
@@ -0,0 +1,11 @@
+filname = "rapport.csv" #Filen som skal sorteres, husk å endre navnet slik at det stemmer
+f = open(filname)
+f.readline() #Bli kvitt første linje som bare inneholder
+
+guests = []
+for line in f:
+    values = line.split(",")
+    if values[0].lower() not in guests and values[2] != "Moderator": #Sjekker at eleven ikke er i listen eller moderator
+        guests.append(values[0].lower())
+
+print(guests)
