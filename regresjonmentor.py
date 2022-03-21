c9b0bbf89d370518c617b1a8f4517156a846601e
Carl Thyness <carl.thyness@gmail.com>
2022-03-21 20:24:40 +0100

    Penere plot og litt endring i rekkefølge av hendelser

--- regresjonmentor.py
+++ regresjonmentor.py
@@ -12,7 +12,7 @@ def g(koeffisienter, x):
 
 #plotter generell polynomfunksjon
 def plotFunc(params):
-    x = np.linspace(-5,5,100)
+    x = np.linspace(-5,5,1000)
     plot_y = g(params,x)
     plt.plot(x,plot_y)
     
@@ -20,17 +20,16 @@ def plotFunc(params):
 #plotter punkter (x,y)
 def plotPunkt(x,y):
     plt.plot(x,y,"rx")
-    
-plt.grid()
 
 print("Hei! Nå skal vi lage et andregradspolynom på formen ax\u00b2+bx+c") #\u00b gir "opphøyd i"
 a = float(input("Skriv inn din a: "))
 b = float(input("Skriv inn din b: "))
 c = float(input("Skriv inn din c: "))
 plotFunc([a,b,c])
+plt.grid()
 plt.show()
+
 N = int(input("Skriv inn antall punkter du ønsker: "))
-n_regresjon = int(input("Hvilken orden skal det være på regresjonsfunksjonen?"))
 
 sample_x = [-4.5,4.5]
 
@@ -41,29 +40,18 @@ sample_y = np.round(g([a,b,c],sample_x)) + np.random.uniform(-1,1,N)   #runder a
 
 plotPunkt(sample_x,sample_y)
 plotFunc([a,b,c])
+plt.grid()
 plt.show()
+
+
+n_regresjon = int(input("Hvilken orden skal det være på regresjonsfunksjonen?"))
 
 f_reg = np.polyfit(sample_x,sample_y,n_regresjon) #gir polynomfunksjon av grad n-1 ved regresjonsanalyse
 
 plotPunkt(sample_x,sample_y)
 plotFunc([a,b,c])
 plotFunc(f_reg)
+plt.grid()
 plt.show()
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-#t = np.linspace(-5,5)
 
 
